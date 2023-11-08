e8665bb65d9531d16978b5b71995347979360665
David Shore <david@foggymedia.com>
2023-11-08 11:33:09 +0100

    first commit

---
+++ question2.js
@@ -0,0 +1,2 @@
+// 2. (3p) Skapa ett inputfält och en knapp på html-sidan. När man klickar på
+// knappen ska det man skrev i input-fältet synas nedanför input-fältet i en div.
---
+++ question3.js
@@ -0,0 +1,2 @@
+// 3. Skapa en div med ett B. Så här: <div>B</div>
+// Lägg det mellan A och C på html-sidan.
