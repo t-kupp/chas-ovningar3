a31e7fb670d14b9438bd4d2f7ef136f39fb6860a
t-kupp <kupper.thorge@gmail.com>
2024-11-06 09:39:05 +0100

    q4 done

--- question4.js
+++ question4.js
@@ -4,5 +4,20 @@
 // Så här: <button class="primary">Button1</button> <button class="primary">Button2</button> ...
 // Koden kan använda en for-loop
 
+for (let i = 1; i < 11; i++) {
+  let button = document.createElement("button");
+  button.classList.add("primary");
+  button.innerText = `Button${i}`;
+  document.body.appendChild(button);
+}
+
 // 4.2 (2p) Lägg till addEventlistener på varje button.
 // Vid eventet "click" på button ska klassen ändras till "secondary"
+
+const buttons = document.querySelectorAll(".primary");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.classList.remove("primary");
+    btn.classList.add("secondary");
+  });
+});
