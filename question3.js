db9a69f59639307bf6bbd1f6883cf935bc3b6564
t-kupp <kupper.thorge@gmail.com>
2024-11-06 09:30:02 +0100

    q3 done

--- question3.js
+++ question3.js
@@ -1,2 +1,11 @@
 // 3. Skapa en div med ett B. Så här: <div>B</div>
 // Lägg det mellan A och C på html-sidan.
+
+const C = document.querySelector("#C");
+const myElement = document.querySelector("#my-element");
+
+const B = document.createElement("div");
+B.id = "B";
+B.innerText = "B";
+
+myElement.insertBefore(B, C);
